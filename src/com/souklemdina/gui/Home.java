f2f35e1bc97ee9d937b37abfb928290c1f67430d
Ben Hassouna <nihel.benhassouna@esprit.tn>
2018-04-25 15:53:48 +0100

    ConnectedUser 100%

--- src/com/souklemdina/gui/Home.java
+++ src/com/souklemdina/gui/Home.java
@@ -24,6 +24,7 @@ import com.codename1.ui.events.ActionEvent;
 import com.codename1.ui.events.ActionListener;
 import com.codename1.ui.layouts.BoxLayout;
 import com.codename1.ui.plaf.Style;
+import com.codename1.ui.plaf.UIManager;
 import com.codename1.ui.util.Resources;
 import com.souklemdina.entities.Produit;
 import static com.souklemdina.gui.Authentification.connectedUser;
@@ -43,15 +44,17 @@ public class Home {
     Label test;
     Label titre;
     Label prix;
-    Label image ;
+    Label image;
     Label categorie;
     Label quantite;
     Label description;
     ConnectionRequest connectionRequest;
+    Resources theme;
+
     public Home() {
-      //  System.out.println(connectedUser.getNom());
+        theme = UIManager.initFirstTheme("/theme");
         f = new Form(BoxLayout.y());
-Container cnt = new Container();
+        Container cnt = new Container();
         test = new Label();
         lb = new SpanLabel("");
 
@@ -59,14 +62,12 @@ Container cnt = new Container();
         ArrayList<Produit> l = ps.getList2();
         for (int i = 0; i < l.size(); i++) {
 
-            
             cnt.add(addItem(l.get(i)));
-            
+
         }
-        
+
         f.add(cnt);
- 
-       
+
     }
 
 //fonction traja3 container yekhou produit en parametre w kol mara 3aytelha hne    
@@ -75,29 +76,34 @@ Container cnt = new Container();
         categorie = new Label(p.getCategorie());
         prix = new Label(p.getPrix().toString());
         image = new Label(p.getPhoto().toString());
-                Label label = new Label();
+        Label label = new Label();
 
-                 Button btn = new Button("detail produit");
- int deviceWidth = Display.getInstance().getDisplayWidth() / 4;
-                                Image placeholder = Image.createImage(deviceWidth, deviceWidth); //square image set to 10% of screen width
-                                EncodedImage encImage = EncodedImage.createFromImage(placeholder, false);
-                                label.setIcon(URLImage.createToStorage(encImage,
-                                        "Large_" + "http://localhost/SoukLemdinaPiDev/web/uploads/images/"+p.getPhoto()+
-                                                "", "http://localhost/SoukLemdinaPiDev/web/uploads/images/"+p.getPhoto()+
-                                                        "", URLImage.RESIZE_SCALE_TO_FILL));
+        Button btn = new Button("detail produit");
+        int deviceWidth = Display.getInstance().getDisplayWidth() / 4;
+        Image placeholder = Image.createImage(deviceWidth, deviceWidth); //square image set to 10% of screen width
+        EncodedImage encImage = EncodedImage.createFromImage(placeholder, false);
+        label.setIcon(URLImage.createToStorage(encImage,
+                "Large_" + "http://localhost/SoukLemdinaPiDev/web/uploads/images/" + p.getPhoto()
+                + "", "http://localhost/SoukLemdinaPiDev/web/uploads/images/" + p.getPhoto()
+                + "", URLImage.RESIZE_SCALE_TO_FILL));
         Container cnt1 = new Container(BoxLayout.y());
         Container cnt2 = new Container(BoxLayout.x());
         cnt1.add(label);
         cnt1.add(titre);
         cnt1.add(categorie);
         cnt1.add(prix);
-         cnt1.add(btn);
+        cnt1.add(btn);
 
         cnt2.add(cnt1);
         cnt2.setLeadComponent(btn);
 
-        btn.addActionListener((l)->{
-            Info(p).show();
+        btn.addActionListener(new ActionListener() {
+
+            public void actionPerformed(ActionEvent l) {
+                //  ToolBarCustom tbs = new ToolBarCustom();
+              
+                Info(p).show();
+            }
         });
         return cnt2;
     }
@@ -111,25 +117,26 @@ Container cnt = new Container();
     }
 
     private Form Info(Produit p) {
-       Form f = new Form();
+        Form f = new Form();
 
-        
         titre = new Label(p.getTitre());
         categorie = new Label(p.getCategorie());
         prix = new Label(p.getPrix().toString());
         description = new Label(p.getDescription().toString());
         Label label = new Label();
-         int deviceWidth = Display.getInstance().getDisplayWidth() / 4;
-                                Image placeholder = Image.createImage(deviceWidth, deviceWidth); //square image set to 10% of screen width
-                                EncodedImage encImage = EncodedImage.createFromImage(placeholder, false);
-                                label.setIcon(URLImage.createToStorage(encImage,
-                                        "Large_" + "http://localhost/SoukLemdinaPiDev/web/uploads/images/"+p.getPhoto()+
-                                                "", "http://localhost/SoukLemdinaPiDev/web/uploads/images/"+p.getPhoto()+
-                                                        "", URLImage.RESIZE_SCALE_TO_FILL));
+        int deviceWidth = Display.getInstance().getDisplayWidth() / 4;
+        Image placeholder = Image.createImage(deviceWidth, deviceWidth); //square image set to 10% of screen width
+        EncodedImage encImage = EncodedImage.createFromImage(placeholder, false);
+        label.setIcon(URLImage.createToStorage(encImage,
+                "Large_" + "http://localhost/SoukLemdinaPiDev/web/uploads/images/" + p.getPhoto()
+                + "", "http://localhost/SoukLemdinaPiDev/web/uploads/images/" + p.getPhoto()
+                + "", URLImage.RESIZE_SCALE_TO_FILL));
         Button btnn = new Button("Ajout produit");
-        btnn.addActionListener(e->{
-        AddProduct ap = new AddProduct();
-        ap.getF().show();
+        btnn.addActionListener(e -> {
+            ToolBarCustom tbs = new ToolBarCustom();
+            AddProduct ap = new AddProduct();
+            ap.setF(tbs.Customize(ap.getF()));
+            ap.getF().show();
         });
         Container cnt1 = new Container(BoxLayout.y());
         Container cnt2 = new Container(BoxLayout.x());
@@ -140,9 +147,9 @@ Container cnt = new Container();
         cnt1.add(label);
         cnt1.add(btnn);
         cnt2.add(cnt1);
-       f.add(cnt2);
-       
-        return f ;
+        f.add(cnt2);
+
+        return f;
 
     }
 
